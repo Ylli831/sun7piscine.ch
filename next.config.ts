606d37519b1b6fb064829870a7d2db48ce155ca9
ylli8822@gmail.com <ylli8822@gmail.com>
2025-10-18 23:51:27 +0200

    Fixed images on prod

--- next.config.ts
+++ next.config.ts
@@ -31,6 +31,7 @@ const nextConfig: NextConfig = {
               "default-src 'self'",
               "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://static.cloudflareinsights.com https://challenges.cloudflare.com",
               "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
+              "style-src-elem 'self' 'unsafe-inline' https://fonts.googleapis.com",
               "img-src 'self' data: blob: https:",
               "font-src 'self' data: https://fonts.gstatic.com",
               "connect-src 'self' https://cloudflareinsights.com https://*.cloudflare.com",
